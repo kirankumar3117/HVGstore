a7d94a689902e735e5f6ccb7c88af62fb806476f
kiran kumar <ch.kirankumar311.com>
2023-01-28 11:14:44 +0530

    login_functinality

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
 import styled from "styled-components";
 import './App.css';
+import {IoMdRemoveCircleOutline,IoMdAddCircleOutline} from "react-icons/io";
+import {AiOutlineDelete} from "react-icons/ai"
+import LoginData from "./components/LoginData";
 
 function App() {
   return (
@@ -8,7 +11,13 @@ function App() {
     
       <Container> 
         <Nav>
-          <Login>
+         <Select name="cars" id="cars">
+          <option value="volvo">Volvo</option>
+          <option value="saab">Saab</option>
+          <option value="opel">Opel</option>
+          <option value="audi">Audi</option>
+         </Select>
+          <Login >
             Login
           </Login>
         </Nav>
@@ -23,15 +32,23 @@ function App() {
           </AddingItems>
           <Item>
             <Top>
-            <ItemName>Kaju</ItemName>
-            <In>In</In>
-            <Out>Out</Out>
+            <ItemName>
+              <Name>
+                Kaju
+              </Name>
+              <Quantity>
+                500000
+              </Quantity>
+
+            </ItemName>
+            <In>
+              <IoMdAddCircleOutline size={30}/>
+            </In>
+            <Out>
+              <IoMdRemoveCircleOutline size={30}/>
+            </Out>
             </Top>
-            <Bottom>
-              <Delete>
-                Delete
-              </Delete>
-            </Bottom>
+          
 
           </Item>
         </Main>
@@ -68,9 +85,25 @@ const Title=styled.div`
 const Nav=styled.nav`
   display:flex;
   flex-direction:row;
-  justify-content:flex-end;
+  justify-content:space-between;
   width:100vw;
   height:40px;
+`;
+const Select=styled.select`
+   background-color: rgba(0, 0, 0, 0.6);
+    padding:8px 16px;
+    text-decoration:uppercase;
+    letter-spacing:1.5px;
+    border:1px solid #f9f9f9;
+    border-radius:4px;
+    transition:all 0.2s ease 0s;
+    margin:10px  0 0 15px;
+    height:36px;
+    color:white;
+    padding:0 10px;
+    cursor:pointer;
+   
+    
 `;
 
 const Login=styled.div`
@@ -92,7 +125,6 @@ const Login=styled.div`
 `;
 
 const Main=styled.section`
-  border:1px solid #f9f9f9;
   width:60vw;
   margin-left:20vw;
   height:200px;
@@ -122,12 +154,16 @@ const AddingItems=styled.div`
 
 const Item=styled.div`
   width:100%;
-  height:80px;
-  background-color: white;
+  height:40px;
+  background-color:#90e78a;
   color:black;
   margin-top:15px;
   display:flex;
   flex-direction: column;
+  border-radius:4px;
+  border:2px solid white;
+  overflow: hidden;
+  cursor:pointer;
 `;
 
 const Top=styled.div`
@@ -135,15 +171,43 @@ const Top=styled.div`
   height:40px;
   display: flex;
   flex-direction: row;
-  gap:5px;
-  justify-content:space-around;
+  justify-content:space-between;
 `;
 
-const ItemName=styled.div``;
-const In=styled.div``;
-const Out=styled.div``;
-const Bottom=styled.div``;
-const Delete=styled.div``;
+const ItemName=styled.div` 
+  width:88%; 
+  display:flex;
+  flex-direction:row;
+  justify-content:space-between;
+  text-align:center;
+  background-color:#5ba2e0;
+  border-radius:4px;
+`;
+const Name=styled.div`
+  padding:10px 5px;
+  font-size:19px;
+  font-weight:bold;
+  text-align:center;
+
+`;
+const Quantity=styled.div`
+  font-size:19px;
+  padding:10px 0px;
+  text-align: center;
+`;
+const In=styled.div`
+  width:50px;
+  background-color: #90e78a;
+  padding:5px 10px;
+`;
+const Out=styled.div`
+  width:50px;
+  background-color:#e78a98;
+  border-radius:4px;
+  padding:5px 10px;
+`;
+
+
 
 
 
